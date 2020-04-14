a0d8982c9dadaafaff029067243a395862488010
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-04-13 18:22:46 -0600

    Used module CustomDefaults.py for uniformity purposes

--- Day 3: Log In GUI/login.py
+++ Day 3: Log In GUI/login.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python
 
 import tkinter as tk
+import json
+import CustomDefaults
 
 class Login(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Login')
         self.configure(bg='lightgrey')
-        self.geometry('450x250')
+        self.geometry('250x300')
+        self.resizable(False, False)
         
         ## Login Frame ##
         # Log In banner
         self.banner = tk.Label(text='Log In', bg='lightgrey', fg='black')
-        self.banner.configure(font=('Arial Black', 30))
-        self.banner.grid(row=0, column=0, sticky='new')
-        # Username and Password
+        self.banner.configure(font=('Comic Sans', 30))
+        self.banner.grid(row=0, column=0, sticky='N')
+        # Username
         self.loginFrame = tk.Frame(bg='lightgrey')
-        self.userName = DefaultLabel(self.loginFrame, 'Username: ', column=0)
-        self.userEntry = DefaultEntry(self.loginFrame, column=1)
+        self.userName = CustomDefaults.DefaultLabel(self.loginFrame, row=1, text='Username: ')
+        self.userEntry = CustomDefaults.DefaultEntry(self.loginFrame, row=1)
         self.userEntry.focus_set()
-        self.passwordLabel = DefaultLabel(self.loginFrame, 'Password: ', column=2)
-        self.passwordEntry = DefaultEntry(self.loginFrame, column=3, show='*')
+        # Password
+        self.passwordLabel = CustomDefaults.DefaultLabel(self.loginFrame, row=2, text='Password: ')
+        self.passwordEntry = CustomDefaults.DefaultEntry(self.loginFrame, row=2, show='*')
+        # Login Button
         self.loginButton = tk.Button(self.loginFrame, text='Log In!', command=self.Login)
         self.loginButton.configure(bg='lightgrey', fg='black', font=('Arial', 8))
-        self.loginButton.grid(row=2, column=0, padx=5, pady=10, ipady=5, sticky='NEWS')
+        self.loginButton.grid(row=3, column=1, padx=5, pady=10, ipady=5, sticky='NESW')
         self.bind('<Return>', self.Login)
         self.loginFrame.grid(row=1, column=0, sticky='news')
         ## End Login Frame ##
@@ -35,21 +40,6 @@ class Login(tk.Tk):
                 'passwd': self.passwordEntry.get()
             }
             print(account)
-
-class DefaultLabel(tk.Label):
-    def __init__(self, master, text, column):
-        super().__init__(master, text=text)
-        self.configure(bg='lightgrey', fg='black', font=('Arial', 8))
-        self.grid(row=1, column=column, padx=5, pady=10, ipady=5, sticky='NESW')
-
-class DefaultEntry(tk.Entry):
-    def __init__(self, master, column, show=''):
-        super().__init__(master, show=show)
-        self.configure(bg='lightgrey', fg='black')
-        self.configure(justify='center', font=('Arial', 8))
-        self.grid(row=1, column=column, padx=5, pady=10, ipady=5, sticky='NESW')
-
-
 
 if __name__ == '__main__':
     login = Login()

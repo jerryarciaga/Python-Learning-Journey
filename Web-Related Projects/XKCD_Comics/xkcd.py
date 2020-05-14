99d1cb173f65193142854a92e111fc591106b15e
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-05-14 13:43:36 -0600

    Program initially can return xkcd comic titles

--- Web-Related Projects/XKCD_Comics/xkcd.py
+++ Web-Related Projects/XKCD_Comics/xkcd.py
@@ -3,15 +3,19 @@
 import requests, bs4
 import os
 
-## Create folder for XKCD
-#if not os.access('XKCD', os.F_OK):
-#    os.mkdir('XKCD')
-#os.chdir('XKCD')
+# Create folder for XKCD
+if not os.access('XKCD', os.F_OK):
+    os.mkdir('XKCD')
+os.chdir('XKCD')
 
-xkcd_request = requests.get('https://xkcd.com/')
+xkcd_site = 'https://xkcd.com/'
+xkcd_request = requests.get(xkcd_site)
 while xkcd_request.status_code == 200:
     xkcd_soup = bs4.BeautifulSoup(xkcd_request.text, 'html.parser')
     title = xkcd_soup.select('#ctitle')
-    print(title[0].text)
-    prev = xkcd_soup.select('#middleContainer > ul:nth-child(2) > li:nth-child(2) > a')
-    xkcd_request = requests.get(f'''https://xkcd.com{prev[0].get('href')}''')
+    print(f'Downloading {title[0].text}')
+    # TODO Perform download
+    prev_link = xkcd_site + xkcd_soup.select(f'''#middleContainer > 
+        ul:nth-child(2) > li:nth-child(2) > a
+        ''')[0].get('href')
+    xkcd_request = requests.get(prev_link)

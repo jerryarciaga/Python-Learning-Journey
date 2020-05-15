d609ea597606c88850c29e35f5c36dd224e6021f
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-05-14 19:20:59 -0600

    First commit for manga downloading program

---
+++ Web-Related Projects/Manga Downloader/manga_downloader.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import requests, bs4
+import os, sys
+
+class Manga():
+    
+
+# Create directory for storage
+if not os.isdir('Manga Collection')
+    os.mkdir('Manga Collection')
+chdir('Manga Collection')
+
+if len(sys.argv == 1):
+    print('Please type the title you want to save')
+else:
+    
